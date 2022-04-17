94e25a96928b32e5c8da2a3d21993d9c6d6fbcae
“Ahad <ahussain@dev-10.com>
2022-04-17 01:28:09 -0400

    Implemented weather function

--- holiday-manager.py
+++ holiday-manager.py
@@ -5,13 +5,6 @@ from bs4 import BeautifulSoup
 import requests
 
 
-# -------------------------------------------
-# Modify the holiday class to 
-# 1. Only accept Datetime objects for date.
-# 2. You may need to add additional functions
-# 3. You may drop the init if you are using @dataclasses
-# --------------------------------------------
-
 class Holiday:
     def __init__(self,holiday_name,date):
         self.__holiday_name = holiday_name
@@ -34,12 +27,6 @@ class Holiday:
 
 
 
-# # -------------------------------------------
-# # The HolidayList class acts as a wrapper and container
-# # For the list of holidays
-# # Each method has pseudo-code instructions
-# # --------------------------------------------
-
 class HolidayList:
 
     def __init__(self):
@@ -50,8 +37,7 @@ class HolidayList:
             raise TypeError("\nHoliday must be a Holiday Object!\n")
         else:
             self.innerHolidays.append(holidayObj)
-            print(f"\nSuccess:\n{holidayObj} has been added to the holiday list.\n")
-
+            print(f"Success:\n{holidayObj} has been added to the holiday list.\n")
         # Make sure holidayObj is an Holiday Object by checking the type
         # Use innerHolidays.append(holidayObj) to add holiday
         # print to the user that you added a holiday
@@ -62,8 +48,10 @@ class HolidayList:
             if HolidayName == holiday.get_name() and Date == holiday.get_date():
                 found_holiday = holiday
         return found_holiday
+
         # Find Holiday in innerHolidays
         # Return Holiday
+
 
     def removeHoliday(self, HolidayName, Date):
         holiday = self.findHoliday(HolidayName, Date)
@@ -71,13 +59,15 @@ class HolidayList:
             print(f"Error:\n{HolidayName} not found.\n")
         else:
             self.innerHolidays.remove(holiday)
-            print(f"\nSuccess:\n{HolidayName} has been removed from the holiday list.\n")
+            print(f"Success:\n{HolidayName} has been removed from the holiday list.\n")
             
         # Find Holiday in innerHolidays by searching the name and date combination.
         # remove the Holiday from innerHolidays
         # inform user you deleted the holiday
 
+
     def read_json(self, filelocation):
+        count = 0
         with open(filelocation) as f:
             holidays_dict = json.load(f)
             for i in range(len(holidays_dict['holidays'])):
@@ -86,11 +76,14 @@ class HolidayList:
                 holiday_date = datetime.date.fromisoformat(holiday_date)
                 holiday = Holiday(holiday_name, holiday_date)
                 self.addHoliday(holiday)
+                count += 1
             f.close()
-
+        print(f'{count} Holiday(s) loaded from file "{filelocation}"\n')
+            
         # Read in things from json file location
         # Use addHoliday function to add holidays to inner list.
     
+
     def save_to_json(self, filelocation):
         master_holiday_list = []
         holiday_dict = {}
@@ -99,7 +92,9 @@ class HolidayList:
             holiday_date = str(self.innerHolidays[i].get_date())
             one_holiday = {"name": holiday_name,"date": holiday_date}
             master_holiday_list.append(one_holiday)
+
         master_holiday_list = sorted(master_holiday_list, key = lambda x: (x['date'], x['name']))
+
         holiday_dict = {'holidays': master_holiday_list}
         updated_holiday_json = json.dumps(holiday_dict, indent=3, separators=(',', ': '))
         
@@ -108,14 +103,13 @@ class HolidayList:
             outfile.close()
 
          # Write out json file to selected file.
-        
+
     def Scraped_Holiday_List(self, URL, year):
             
         try:
             html = requests.get(URL)
             html.raise_for_status()
-            html = requests.get(URL).text
-
+            html = html.text
         except requests.exceptions.HTTPError as err:
             print(err)
 
@@ -124,35 +118,30 @@ class HolidayList:
         # Remove Holidays with Tentative Dates
         for tentative_date in soup.select('a:-soup-contains("(Tentative Date)")'):
             tentative_date.decompose()
-            
 
         # Find Table with Data
         table = soup.find('article',attrs = {'class':'table-data'})
         for row in table.find_all_next('tbody'): 
             # Find any tag that contains href /holidays/us/
-
             holidays = row.select('a[href *= "/holidays/us/"]')
             for holiday in holidays:
                 holiday_name = holiday.text
 
                 # Date is contained in text in the tag that is 3 tags prior to the href tag
                 date = holiday.find_previous().find_previous().find_previous()
-
-                # 11:17AM is a placeholder needed for the DateTime Object to work
-                holiday_date = date.getText() + f" {year}" + ' 11:17AM'
-                format = '%b %d %Y %I:%M%p'
+                holiday_date = date.getText() + f" {year}"
+                format = '%b %d %Y'
                 holiday_date = datetime.datetime.strptime(holiday_date, format).date()
 
                 # Add Non-Duplicates to Inner List
-                holiday = self.findHoliday(holiday_name, holiday_date)
+                find_holiday = self.findHoliday(holiday_name, holiday_date)
 
-                if holiday is None:
+                if find_holiday is None:
                     # Instantiate Holiday Objects and Add them to List of Objects
                     holidayObj = Holiday(holiday_name, holiday_date)
-                    self.addHoliday(holidayObj)
-                
+                    self.innerHolidays.append(holidayObj)
 
-                
+
     def scrapeHolidays(self):
 
         # Add 2020 Holidays
@@ -175,31 +164,34 @@ class HolidayList:
         URL = "https://www.timeanddate.com/holidays/us/2024"
         self.Scraped_Holiday_List(URL, 2024)
 
+
         # Scrape Holidays from https://www.timeanddate.com/holidays/us/ 
         # Remember, 2 previous years, current year, and 2  years into the future. You can scrape multiple years by adding year to the timeanddate URL. For example https://www.timeanddate.com/holidays/us/2022
         # Check to see if name and date of holiday is in innerHolidays array
         # Add non-duplicates to innerHolidays
-        # Handle any exceptions.     
-
+        # Handle any exceptions.
+        
     def numHolidays(self):
         return len(self.innerHolidays)
         # Return the total number of holidays in innerHolidays
-    
-    def filter_holidays_by_week(self,year, week_number):
-        holidays = list(filter(lambda x: x.get_date().isocalendar()[1] == week_number \
-            and x.get_date().isocalendar()[0] == year, self.innerHolidays))
-    
-        return holidays
 
+    def filter_holidays_by_week(self, year, week_number):
+        holidays = list(filter(lambda x: x.get_date().isocalendar()[1] == week_number \
+                and x.get_date().isocalendar()[0] == year, self.innerHolidays))
+        
+        return holidays
+        
         # Use a Lambda function to filter by week number and save this as holidays, use the filter on innerHolidays
         # Week number is part of the the Datetime object
         # Cast filter results as list
         # return your holidays
 
+
     @staticmethod
     def displayHolidaysInWeek(holiday_list):
         for i in range(len(holiday_list)):
             print(holiday_list[i])
+        print()
             
         # Use your filter_holidays_by_week to get list of holidays within a week as a parameter
         # Output formated holidays in the week. 
@@ -214,7 +206,7 @@ class HolidayList:
         today = datetime.datetime.today()
         days_past = datetime.timedelta(days = days_ago)
         date = today - days_past
-
+        
         timestamp = int(date.timestamp())
 
         querystring = {"lat":"26.6406","lon":"-81.8723","dt":timestamp}
@@ -224,49 +216,113 @@ class HolidayList:
             "X-RapidAPI-Key": "a2ba6e432emshba867077d11af9ap1a0278jsnc4e59674ffb8"
         }
 
-        response = requests.get(url, headers=headers, params=querystring).json()
+        try:
+            response = requests.get(url, headers=headers, params=querystring).json()
 
-        unix_timestamp = int(response['current']['dt'])
+            unix_timestamp = int(response['current']['dt'])
 
+        except KeyError:
+            raise KeyError("Limit the Number of API Calls you make! Wait at least 30 seconds and try again.")
+        
         day = datetime.datetime.utcfromtimestamp(unix_timestamp).strftime('%Y-%m-%d')
         weather = response["current"]['weather'][0]['description']
-
+        
         return day, weather
+
 
 
     @staticmethod
     def Get_Forecasted_Weather_Data():
-        url = "https://community-open-weather-map.p.rapidapi.com/forecast/"
+        try:
+            url = "https://community-open-weather-map.p.rapidapi.com/forecast/"
 
-        querystring = {"q":"Fort Myers,us",}
+            querystring = {"q":"Fort Myers,us",}
 
-        headers = {
-            "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com",
-            "X-RapidAPI-Key": "a2ba6e432emshba867077d11af9ap1a0278jsnc4e59674ffb8"
-        }
+            headers = {
+                "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com",
+                "X-RapidAPI-Key": "a2ba6e432emshba867077d11af9ap1a0278jsnc4e59674ffb8"
+            }
 
-        response = requests.get(url, headers=headers, params=querystring).json()
-        timestamp = time.strftime('%H:%M:%S')
-        time_list = ['24:00:00','03:00:00','06:00:00','09:00:00','12:00:00','15:00:00','18:00:00','21:00:00']
+            response = requests.get(url, headers=headers, params=querystring).json()
 
-        for i in range(len(time_list)):
-            if str(timestamp) <= time_list[i]:
-                future_timestamp = time_list[i]
-            elif future_timestamp == '24:00:00':
-                future_time_stamp = '00:00:00'
+            timestamp = time.strftime('%H:%M:%S')
+            time_list = ['24:00:00','03:00:00','06:00:00','09:00:00','12:00:00','15:00:00','18:00:00','21:00:00']
+            future_time_stamp = ""
+            for i in range(len(time_list)):
+                if str(timestamp) <= time_list[i]:
+                    future_timestamp = time_list[i]
+                elif future_timestamp == '24:00:00':
+                    future_time_stamp = '00:00:00'
 
-        weather_list = []
-        date_list = []
+            weather_list = []
+            date_list = []
 
-        for i in range(len(response['list'])):
-            if future_time_stamp in response['list'][i]['dt_txt']:
-                date = response['list'][i]['dt_txt']
-                date = date.replace(f' {future_time_stamp}', '')
-                date_list.append(date)
-                weather = response['list'][i]['weather'][0]['description']
-                weather_list.append(weather)
+            for i in range(len(response['list'])):
+                if future_time_stamp in response['list'][i]['dt_txt']:
+                    date = response['list'][i]['dt_txt']
+                    date = date.replace(f' {future_time_stamp}', '')
+                    date_list.append(date)
+                    weather = response['list'][i]['weather'][0]['description']
+                    weather_list.append(weather)
+
+        except KeyError as err:
+
+            raise err
 
         return weather_list, date_list
+
+
+    def getWeather(self):
+        my_date = datetime.date.today()
+        year, current_week, _ = my_date.isocalendar()
+        holidays = self.filter_holidays_by_week(year, current_week)      
+
+        previous_days = 5
+        previous_days_list = []
+        previous_weather_list = []
+        while previous_days >= 0:
+            day, weather = HolidayList.Get_Previous_Weather_Data(previous_days)
+            previous_days_list.append(day)
+            previous_weather_list.append(weather)
+            previous_days -= 1
+
+        forecasted_weather_list, forecasted_days_list = HolidayList.Get_Forecasted_Weather_Data()
+        days_list = previous_days_list + forecasted_days_list
+        weather_list = previous_weather_list + forecasted_weather_list
+        weather_dict = {}
+        for i in range(len(weather_list)):
+            weather_dict[days_list[i]] = weather_list[i]
+        
+        for i in range(len(holidays)):
+            holiday_date = str(holidays[i].get_date())
+            if holiday_date in weather_dict:
+                print(f"{holidays[i]} - {weather_dict[holiday_date]}")
+            else:
+                print(f"{holidays[i]} - Weather Data not found")
+        print()
+
+        # Convert weekNum to range between two days
+        # Use Try / Except to catch problems
+        # Query API for weather in that week range
+        # Format weather information
+        # Use the Datetime Module to look up current week and year
+        # Use your filter_holidays_by_week function to get the list of holidays 
+        # for the current week/year
+        # Use your displayHolidaysInWeek function to display the holidays in the week
+        # Ask user if they want to get the weather
+        # If yes, use your getWeather function and display results
+
+        
+test = HolidayList()
+test.read_json('holidays.json')
+test.scrapeHolidays()
+test.save_to_json('sample')
+num = test.numHolidays()
+print(num)
+holidays = test.filter_holidays_by_week(2022,15)
+HolidayList.displayHolidaysInWeek(holidays)
+test.getWeather()
+
 
 #     def getWeather(weekNum):
 #         # Convert weekNum to range between two days
